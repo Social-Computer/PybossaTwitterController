8421374d41c2c5bd8c566fe0858a35a9bf8a64aa
Saud Aljaloud <saud.aljaloud@gmail.com>
2016-02-16 20:15:17 +0000

    change most of the methods to have a jsonObject as the return value other than string/boolean Added one insertion into mongoDB

--- src/main/java/sociam/pybossa/ProjectCreator.java
+++ src/main/java/sociam/pybossa/ProjectCreator.java
@@ -3,8 +3,6 @@ package sociam.pybossa;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import org.json.simple.JSONObject;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -12,6 +10,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 public class ProjectCreator {
 	final static Logger logger = Logger.getLogger(ProjectCreator.class);
@@ -23,7 +22,7 @@ public class ProjectCreator {
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 
-		String jsonData = BuildJsonPorject("test8", "test8", "test8");
+		JSONObject jsonData = BuildJsonPorject("test8", "test8", "test8");
 		String url = host + projectDir + api_key;
 		createProject(url, jsonData);
 
@@ -40,12 +39,12 @@ public class ProjectCreator {
 	 * 
 	 * @return Boolean true if its created, false otherwise.
 	 **/
-	public static Boolean createProject(String url, String jsonData) {
-
+	public static JSONObject createProject(String url, JSONObject jsonData) {
+		JSONObject jsonResult = null;
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		try {
 			HttpPost request = new HttpPost(url);
-			StringEntity params = new StringEntity(jsonData);
+			StringEntity params = new StringEntity(jsonData.toString());
 			params.setContentType("application/json");
 			request.addHeader("content-type", "application/json");
 			request.addHeader("Accept", "*/*");
@@ -53,21 +52,26 @@ public class ProjectCreator {
 			request.addHeader("Accept-Language", "en-US,en;q=0.8");
 			request.setEntity(params);
 			HttpResponse response = httpClient.execute(request);
-			if (response.getStatusLine().getStatusCode() == 200 || response.getStatusLine().getStatusCode() == 204) {
-				BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+			if (response.getStatusLine().getStatusCode() == 200
+					|| response.getStatusLine().getStatusCode() == 204) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						(response.getEntity().getContent())));
 				String output;
-				logger.debug("Output from Server ...." + response.getStatusLine().getStatusCode() + "\n");
+				logger.debug("Output from Server ...."
+						+ response.getStatusLine().getStatusCode() + "\n");
 				while ((output = br.readLine()) != null) {
 					logger.debug(output);
+					jsonResult = new JSONObject(output);
 				}
-				return true;
+				return jsonResult;
 			} else {
-				logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
-				return false;
+				logger.error("Failed : HTTP error code : "
+						+ response.getStatusLine().getStatusCode());
+				return null;
 			}
 		} catch (Exception ex) {
 			logger.error(ex);
-			return false;
+			return null;
 		}
 	}
 
@@ -83,61 +87,120 @@ public class ProjectCreator {
 	 *            later!
 	 * @return Json string
 	 */
-	@SuppressWarnings("unchecked")
-	private static String BuildJsonPorject(String name, String shortName, String description) {
+	private static JSONObject BuildJsonPorject(String name, String shortName,
+			String description) {
 
 		JSONObject app2 = new JSONObject();
-		app2.put("task_presenter", "<style type=\"text/css\">\n" + "    .textContainer {\n"
-				+ "        border-radius: 5px;\n" + "        background-color: #f2f2f2;\n" + "        color: #2B9884;\n"
-				+ "        padding: 12px;\n" + "    }\n" + "</style>\n" + "\n"
-				+ "<div id=\"warning\" class=\"modal fade\">\n" + "  <div class=\"modal-dialog\">\n"
-				+ "    <div class=\"modal-content\">\n" + "      <div class=\"modal-header\">\n"
-				+ "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n"
-				+ "        <h4 class=\"modal-title\"></h4>\n" + "      </div>\n" + "      <div class=\"modal-body\">\n"
-				+ "      </div>\n" + "      <div class=\"modal-footer\">\n"
-				+ "        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>\n"
-				+ "      </div>\n" + "    </div><!-- /.modal-content -->\n" + "  </div><!-- /.modal-dialog -->\n"
-				+ "</div><!-- /.modal -->\n" + "\n" + "<div class=\"row skeleton\">\n"
-				+ "    <div class=\"col-xs-8 col-xs-offset-2\">\n"
-				+ "        <h2 id=\"question\">Does this content indicate a real-world event?</h2>\n" + "    </div>\n"
-				+ "    <div class=\"hidden-xs col-sm-2\">\n"
-				+ "        <p style=\"margin-top:30px;\"><span id=\"i18n_working_task\">Task</span> <span id=\"task-id\">#</span></p>\n"
-				+ "    </div>\n" + "</div>\n" + "<div class=\"row\">\n"
-				+ "    <div class=\"top20 col-xs-8 col-xs-offset-2\">\n" + "        <p class=\"textContainer\">\n"
-				+ "            <span id=\"text\"></span>\n" + "        </p>\n" + "    </div>\n"
-				+ "    <div id=\"answer\" class=\"top20 col-xs-8 col-xs-offset-2\">\n"
-				+ "        <button class=\"btn btn-answer\" value=\"yes\">Yes</button>\n"
-				+ "        <button class=\"btn btn-answer\" value=\"no\">No</button>\n" + "    </div>\n" + "</div>\n"
-				+ "<script type=\"text/javascript\">\n" + "(function() {\n" + "// Default language\n"
-				+ "var userLocale = \"en\";\n" + "// Translations\n" + "var messages = {\"en\": {\n"
-				+ "                        \"i18n_working_task\": \"Task\",\n" + "                      },\n"
-				+ "               };\n" + "// Update userLocale with server side information\n"
-				+ "$(document).ready(function(){\n"
-				+ "    userLocale = document.getElementById('PYBOSSA_USER_LOCALE').textContent.trim();\n" + "});\n"
-				+ "function i18n_translate() {\n" + "    var ids = Object.keys(messages[userLocale])\n"
-				+ "    for (i=0; i<ids.length; i++) {\n" + "        console.log(\"Translating: \" + ids[i]);\n"
-				+ "        document.getElementById(ids[i]).innerHTML = messages[userLocale][ids[i]];\n" + "    }\n"
-				+ "}\n" + "pybossa.taskLoaded(function(task, deferred) {\n" + "    deferred.resolve(task);\n" + "});\n"
-				+ "function warn_user(type, msg) {\n" + "    var p = $(\"<p/>\");\n" + "    var title;\n"
-				+ "    if (type === 'info') {\n" + "        title = \"Hi there! Please, read this carefully\"\n"
-				+ "    }\n" + "    if (type === 'error') {\n" + "        title = \"Ooops! Something went wrong!\"\n"
-				+ "    }\n" + "    if (type === 'warning') {\n"
-				+ "        title = \"Hi there! Please, read this carefully\"\n" + "    }\n" + "    p.text(msg);\n"
-				+ "    $(\".modal-title\").text(title);\n" + "    $(\".modal-body\").html(p);\n"
-				+ "    $(\"#warning\").modal();\n" + "}\n" + "pybossa.presentTask(function(task, deferred) {\n"
-				+ "    if ( !$.isEmptyObject(task) ) {\n" + "        i18n_translate();\n"
-				+ "        $('#task-id').html(task.id);\n"
-				+ "        document.getElementById('text').innerHTML = task.info.text;\n"
-				+ "        $('.btn-answer').off('click').on('click', function(evt) {\n"
-				+ "            var $btn = $(this);\n" + "            var answer = $btn.attr(\"value\");\n"
-				+ "            if (typeof answer != 'undefined') {\n"
-				+ "                pybossa.saveTask(task.id, answer).done(function() {\n"
-				+ "                    deferred.resolve();\n" + "                });\n" + "            }\n"
-				+ "            else {\n" + "                warn_user('error', 'Unexpected type of answer.');\n"
-				+ "            }\n" + "        });\n" + "    }\n" + "    else {\n"
-				+ "        $(\".skeleton\").hide();\n"
-				+ "        warn_user('info', 'You have contributed to all available tasks! Thanks!');\n" + "    }\n"
-				+ "});\n" + "pybossa.run('" + shortName + "');\n" + "})();\n" + "</script>");
+		app2.put(
+				"task_presenter",
+				"<style type=\"text/css\">\n"
+						+ "    .textContainer {\n"
+						+ "        border-radius: 5px;\n"
+						+ "        background-color: #f2f2f2;\n"
+						+ "        color: #2B9884;\n"
+						+ "        padding: 12px;\n"
+						+ "    }\n"
+						+ "</style>\n"
+						+ "\n"
+						+ "<div id=\"warning\" class=\"modal fade\">\n"
+						+ "  <div class=\"modal-dialog\">\n"
+						+ "    <div class=\"modal-content\">\n"
+						+ "      <div class=\"modal-header\">\n"
+						+ "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n"
+						+ "        <h4 class=\"modal-title\"></h4>\n"
+						+ "      </div>\n"
+						+ "      <div class=\"modal-body\">\n"
+						+ "      </div>\n"
+						+ "      <div class=\"modal-footer\">\n"
+						+ "        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>\n"
+						+ "      </div>\n"
+						+ "    </div><!-- /.modal-content -->\n"
+						+ "  </div><!-- /.modal-dialog -->\n"
+						+ "</div><!-- /.modal -->\n"
+						+ "\n"
+						+ "<div class=\"row skeleton\">\n"
+						+ "    <div class=\"col-xs-8 col-xs-offset-2\">\n"
+						+ "        <h2 id=\"question\">Does this content indicate a real-world event?</h2>\n"
+						+ "    </div>\n"
+						+ "    <div class=\"hidden-xs col-sm-2\">\n"
+						+ "        <p style=\"margin-top:30px;\"><span id=\"i18n_working_task\">Task</span> <span id=\"task-id\">#</span></p>\n"
+						+ "    </div>\n"
+						+ "</div>\n"
+						+ "<div class=\"row\">\n"
+						+ "    <div class=\"top20 col-xs-8 col-xs-offset-2\">\n"
+						+ "        <p class=\"textContainer\">\n"
+						+ "            <span id=\"text\"></span>\n"
+						+ "        </p>\n"
+						+ "    </div>\n"
+						+ "    <div id=\"answer\" class=\"top20 col-xs-8 col-xs-offset-2\">\n"
+						+ "        <button class=\"btn btn-answer\" value=\"yes\">Yes</button>\n"
+						+ "        <button class=\"btn btn-answer\" value=\"no\">No</button>\n"
+						+ "    </div>\n"
+						+ "</div>\n"
+						+ "<script type=\"text/javascript\">\n"
+						+ "(function() {\n"
+						+ "// Default language\n"
+						+ "var userLocale = \"en\";\n"
+						+ "// Translations\n"
+						+ "var messages = {\"en\": {\n"
+						+ "                        \"i18n_working_task\": \"Task\",\n"
+						+ "                      },\n"
+						+ "               };\n"
+						+ "// Update userLocale with server side information\n"
+						+ "$(document).ready(function(){\n"
+						+ "    userLocale = document.getElementById('PYBOSSA_USER_LOCALE').textContent.trim();\n"
+						+ "});\n"
+						+ "function i18n_translate() {\n"
+						+ "    var ids = Object.keys(messages[userLocale])\n"
+						+ "    for (i=0; i<ids.length; i++) {\n"
+						+ "        console.log(\"Translating: \" + ids[i]);\n"
+						+ "        document.getElementById(ids[i]).innerHTML = messages[userLocale][ids[i]];\n"
+						+ "    }\n"
+						+ "}\n"
+						+ "pybossa.taskLoaded(function(task, deferred) {\n"
+						+ "    deferred.resolve(task);\n"
+						+ "});\n"
+						+ "function warn_user(type, msg) {\n"
+						+ "    var p = $(\"<p/>\");\n"
+						+ "    var title;\n"
+						+ "    if (type === 'info') {\n"
+						+ "        title = \"Hi there! Please, read this carefully\"\n"
+						+ "    }\n"
+						+ "    if (type === 'error') {\n"
+						+ "        title = \"Ooops! Something went wrong!\"\n"
+						+ "    }\n"
+						+ "    if (type === 'warning') {\n"
+						+ "        title = \"Hi there! Please, read this carefully\"\n"
+						+ "    }\n"
+						+ "    p.text(msg);\n"
+						+ "    $(\".modal-title\").text(title);\n"
+						+ "    $(\".modal-body\").html(p);\n"
+						+ "    $(\"#warning\").modal();\n"
+						+ "}\n"
+						+ "pybossa.presentTask(function(task, deferred) {\n"
+						+ "    if ( !$.isEmptyObject(task) ) {\n"
+						+ "        i18n_translate();\n"
+						+ "        $('#task-id').html(task.id);\n"
+						+ "        document.getElementById('text').innerHTML = task.info.text;\n"
+						+ "        $('.btn-answer').off('click').on('click', function(evt) {\n"
+						+ "            var $btn = $(this);\n"
+						+ "            var answer = $btn.attr(\"value\");\n"
+						+ "            if (typeof answer != 'undefined') {\n"
+						+ "                pybossa.saveTask(task.id, answer).done(function() {\n"
+						+ "                    deferred.resolve();\n"
+						+ "                });\n"
+						+ "            }\n"
+						+ "            else {\n"
+						+ "                warn_user('error', 'Unexpected type of answer.');\n"
+						+ "            }\n"
+						+ "        });\n"
+						+ "    }\n"
+						+ "    else {\n"
+						+ "        $(\".skeleton\").hide();\n"
+						+ "        warn_user('info', 'You have contributed to all available tasks! Thanks!');\n"
+						+ "    }\n" + "});\n" + "pybossa.run('" + shortName
+						+ "');\n" + "})();\n" + "</script>");
+
 		JSONObject app = new JSONObject();
 
 		app.put("name", name);
@@ -150,7 +213,7 @@ public class ProjectCreator {
 		// app.put("published", true);
 		app.put("info", app2);
 
-		return app.toJSONString();
+		return app;
 	}
 
 }
--- src/main/java/sociam/pybossa/TaskCreator.java
+++ src/main/java/sociam/pybossa/TaskCreator.java
@@ -2,6 +2,9 @@ package sociam.pybossa;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -10,7 +13,12 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.json.simple.JSONObject;
+import org.bson.Document;
+import org.json.JSONObject;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
 
 public class TaskCreator {
 
@@ -20,26 +28,40 @@ public class TaskCreator {
 	public static String projectDir = "/api/task";
 	public static String api_key = "?api_key=acc193bd-6930-486e-9a21-c80005cbbfd2";
 
+	static final String mongoHost = "localhost";
+	static final int port = 27017;
+	static final String databaseName = "RECOIN_bins";
+	static final String collectionName = "tasks";
+	final static MongoClient mongoClient = new MongoClient(mongoHost, port);
+	final static MongoDatabase database = mongoClient.getDatabase(databaseName);
+
+	final static SimpleDateFormat MongoDBformatter = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss");
+	final static SimpleDateFormat PyBossaformatter = new SimpleDateFormat(
+			"yyyy-mm-dd'T'hh:mm:ss.SSSSSS");
+
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		String jsonData = BuildJsonTaskContent("#Zika News: what is this https://t.co/tYqAYlbPlc #PathogenPosse",
+		JSONObject jsonData = BuildJsonTaskContent(
+				"#Zika NewsTests5555: what is this https://t.co/tYqAYlbPlc #PathogenPosse",
 				"30", "0", "0", "26", "0.0");
 		// String jsonData = "{\"info\": {\"text\": \"#Zika News: Stop The Zika
 		// Virus https://t.co/tYqAYlbPlc #PathogenPosse\"}, \"n_answers\": 30,
 		// \"quorum\": 0, \"calibration\": 0, \"project_id\": 11,
 		// \"priority_0\": 0.0}";
 		String url = host + projectDir + api_key;
-		createProject(url, jsonData);
-
+		JSONObject response = createTask(url, jsonData);
+		System.out.println(response.toString());
+		System.out.println(insertTaskIntoPyBossa(response, false));
 	}
 
-	public static Boolean createProject(String url, String jsonData) {
-
+	public static JSONObject createTask(String url, JSONObject jsonData) {
+		JSONObject jsonResult = null;
 		HttpClient httpClient = HttpClientBuilder.create().build();
 
 		try {
 			HttpPost request = new HttpPost(url);
-			StringEntity params = new StringEntity(jsonData);
+			StringEntity params = new StringEntity(jsonData.toString());
 			params.setContentType("application/json");
 			request.addHeader("content-type", "application/json");
 			request.addHeader("Accept", "*/*");
@@ -48,21 +70,26 @@ public class TaskCreator {
 			request.setEntity(params);
 
 			HttpResponse response = httpClient.execute(request);
-			if (response.getStatusLine().getStatusCode() == 200 || response.getStatusLine().getStatusCode() == 204) {
-				BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+			if (response.getStatusLine().getStatusCode() == 200
+					|| response.getStatusLine().getStatusCode() == 204) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						(response.getEntity().getContent())));
 				String output;
-				logger.debug("Output from Server ...." + response.getStatusLine().getStatusCode() + "\n");
+				logger.debug("Output from Server ...."
+						+ response.getStatusLine().getStatusCode() + "\n");
 				while ((output = br.readLine()) != null) {
 					logger.debug(output);
+					jsonResult = new JSONObject(output);
 				}
-				return true;
+				return jsonResult;
 			} else {
-				logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
-				return false;
+				logger.error("Failed : HTTP error code : "
+						+ response.getStatusLine().getStatusCode());
+				return null;
 			}
 		} catch (Exception ex) {
 			logger.error(ex);
-			return false;
+			return null;
 		}
 
 	}
@@ -80,8 +107,8 @@ public class TaskCreator {
 	 * @param priority_0
 	 * @return Json string
 	 */
-	@SuppressWarnings("unchecked")
-	private static String BuildJsonTaskContent(String text, String n_answers, String quorum, String calibration,
+	private static JSONObject BuildJsonTaskContent(String text,
+			String n_answers, String quorum, String calibration,
 			String project_id, String priority_0) {
 
 		JSONObject app = new JSONObject();
@@ -94,7 +121,69 @@ public class TaskCreator {
 		app2.put("project_id", project_id);
 		app2.put("priority_0", priority_0);
 
-		return app2.toJSONString();
+		return app2;
+	}
+
+	private static Boolean insertTaskIntoPyBossa(JSONObject response, Boolean isPushed) {
+
+		try {
+			Integer pybossa_task_id = response.getInt("id");
+			String created_String = response.getString("created");
+			Date publishedAt = PyBossaformatter.parse(created_String);
+			String targettedFormat = MongoDBformatter.format(publishedAt);
+			Integer project_id = response.getInt("project_id");
+			JSONObject info = response.getJSONObject("info");
+			String task_text = info.getString("text"); 
+			logger.debug("Inserting a task into MongoDB");
+			if (pushTask(pybossa_task_id, targettedFormat, project_id,
+					isPushed, task_text)) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (ParseException e) {
+			logger.error(e);
+		}
+
+		return true;
+	}
+
+	private static boolean pushTask(Integer pybossa_task_id,
+			String publishedAt, Integer project_id, Boolean isPushed,
+			String task_text) {
+
+		try {
+			if (publishedAt != null && project_id != null && isPushed != null
+					&& task_text != null) {
+
+				FindIterable<Document> iterable = database.getCollection(
+						collectionName).find(
+						new Document("project_id", project_id).append(
+								"task_text", task_text));
+				if (iterable.first() == null) {
+					database.getCollection(collectionName).insertOne(
+							new Document()
+									.append("pybossa_task_id", pybossa_task_id)
+									.append("publishedAt", publishedAt)
+									.append("project_id", project_id)
+									.append("isPushed", isPushed)
+									.append("task_text", task_text));
+					logger.info("One task is inserted");
+
+				} else {
+					logger.error("task is already in the collection!!");
+				}
+
+			}
+			return true;
+		} catch (Exception e) {
+			logger.error("Error with inserting the task " + "pybossa_task_id "
+					+ pybossa_task_id + "publishedAt " + publishedAt
+					+ "project_id " + project_id + "isPushed " + isPushed
+					+ "task_text " + task_text + "\n" + e);
+			return false;
+		}
+
 	}
 
 }
