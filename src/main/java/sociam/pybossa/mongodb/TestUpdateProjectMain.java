6c8a476ab4ee595ca19c7d21634bdf240280604c
SaudAljaloud <sza1g10@ecs.soton.ac.uk>
2016-02-18 23:27:47 +0000

    no message

--- src/main/java/sociam/pybossa/mongodb/TestUpdateProjectMain.java
+++ src/main/java/sociam/pybossa/mongodb/TestUpdateProjectMain.java
@@ -21,10 +21,27 @@ public class TestUpdateProjectMain {
 
 	public static void main(String[] args) {
 
+		UpdateResult result = database.getCollection(Config.projectCollection).updateMany(new Document(),
+				new Document().append("$set", new Document("project_id", "")));
+		System.out.println(result.toString());
+
+	}
+
+	public static void updateBasedOnField() {
 		UpdateResult result = database.getCollection(Config.projectCollection).updateMany(
 				new Document("project_name", "healthnostics"),
 				new Document().append("$set", new Document("project_started", "false")));
 		System.out.println(result.toString());
+	}
+
+	public static void cleanProjectList() {
+		UpdateResult result = database.getCollection(Config.projectCollection).updateMany(new Document(),
+				new Document().append("$set", new Document("project_id", "")));
+		System.out.println(result.toString());
+
+		UpdateResult result2 = database.getCollection(Config.projectCollection).updateMany(new Document(),
+				new Document().append("$set", new Document("project_started", "false")));
+		System.out.println(result2.toString());
 
 	}
 
