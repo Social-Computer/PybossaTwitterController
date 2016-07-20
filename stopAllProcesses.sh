7b2dde5749b68a45c1f9fdcd0de2dfd3546ba9b0
Saud Aljaloud <saud.aljaloud@gmail.com>
2016-07-20 18:18:00 +0100

    no message

--- stopAllProcesses.sh
+++ stopAllProcesses.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
+!/bin/bash
 set -e
 ps -ef | grep "recoin.mongodb_version"
 read -p "Are you sure you want to KILL the above processes? (Y/n) : " answer
-if [ "$answer" == "Y" ]; then
+if [ "$answer" = "Y" ]; then
 	echo "Preperting to kill these processes: "
-	numberProcess=($(ps -ef | grep "recoin.mongodb_version" | wc -l))
+	numberProcess=$(ps -ef | grep "recoin.mongodb_version" | wc -l)
 	echo "There are $numberProcess Processes"
-	if (($numberProcess > 1)); then
-		ps -ef | grep "recoin.mongodb_version" | grep -v grep | awk '{print $2}' | xargs kill -9
-		echo "Processes are killed!!"
-	else
-		echo "No processes were found!!"
-	fi
+	ps -ef | grep "recoin.mongodb_version" | grep -v grep | awk '{print $2}' | xargs kill -9
+	echo "Processes are killed!!"
 else
 	echo "No actions were taken :)"
 fi
