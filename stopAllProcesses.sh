15c80070820710720f57cae0d089cb252fe4077a
Saud Aljaloud <saud.aljaloud@gmail.com>
2016-07-20 17:59:22 +0100

    no message

--- stopAllProcesses.sh
+++ stopAllProcesses.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 read -p "Are you sure you want to KILL all processes related to TwitterCrowdSourcingController (Y/n) : " answer
-if [ "$answer" == "Y" ] then
-	ps -ef | grep "recoin.mongodb_version" | grep -v grep | awk '{print $2}' | xargs kill -9
+if [ "$answer" == "Y" ]; then
+	echo "Preperting to kill these processes: "
+	ps -ef | grep "recoin.mongodb_version"
+	numberProcess=($(ps -ef | grep "recoin.mongodb_version" | wc -l))
+	echo "There are $numberProcess Processes"
+	if (($numberProcess > 1)); then
+		ps -ef | grep "recoin.mongodb_version" | grep -v grep | awk '{print $2}' | xargs kill -9
+		echo "Processes are killed!!"
+	else
+		echo "No processes were found!!"
+	fi
 else
 	echo "No actions were taken :)"
 fi
